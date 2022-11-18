6f5b358dabec9e8325b2418a4049cc4d8621e108
Cobo <marc.cobo@solera.com>
2022-11-18 12:33:27 +0100

    Added mcv project and removed api

---
+++ mvc-aspnet/mvc-aspnet/Models/Owner.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace mvc_aspnet.Models;
+
+public partial class Owner
+{
+    public int Id { get; set; }
+
+    public string? FirstName { get; set; }
+
+    public string? LastName { get; set; }
+
+    public string? DriverLicense { get; set; }
+
+    public virtual ICollection<Vehicle> Vehicles { get; } = new List<Vehicle>();
+}
